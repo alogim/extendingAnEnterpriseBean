8165c5a0ffc6d77b870c3a31d920c40cab3d24dc
Michael <ubuntuser93@gmail.com>
2016-10-11 09:39:59 +0200

    Initial commit

---
+++ Server/Server-ejb/src/java/server/ServerBean.java
@@ -0,0 +1,11 @@
+package server;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class ServerBean implements ServerBeanRemote
+{
+
+  // Add business logic below. (Right-click in editor and choose
+  // "Insert Code > Add Business Method")
+}
